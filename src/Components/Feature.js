f706222f6b783cd708f61653e37086be4d9b9629
Gaurav Pawar <gauravspawar9624@gmail.com>
2023-06-29 20:51:41 +0530

    mobile UI

--- src/Components/Feature.js
+++ src/Components/Feature.js
@@ -5,12 +5,14 @@ function Feature() {
     <section>
       <div className="feature-container">
         <div className="feature">
-          <h2>YOU DON'T HAVE TO EAT LESS — YOU JUST HAVE TO EAT RIGHT</h2>
-          <span className="description">
+          <div>
+            <h2>YOU DON'T HAVE TO EAT LESS — YOU JUST HAVE TO EAT RIGHT</h2>
+          </div>
+          <div className="description">
             We're Salad-Bar, a new premium food service. We know you're busy and
             find no time to cook. But don't worry—relax, and let us take care of
             that. We happen to be really good at it!
-          </span>
+          </div>
         </div>
       </div>
     </section>
